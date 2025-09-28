b4e17b2d43f11cc883cc6176f860f134c7871177
Arshad <bscs22069@itu.edu.pk>
2025-09-28 22:11:48 +0500

    Add all AST node classes (FunctionDecl, VarDecl, Assignment, BinaryOp, UnaryOp, etc.)

--- arshad-69/parser.cpp
+++ arshad-69/parser.cpp
@@ -119,3 +119,341 @@ public:
         cout << "Param { type: " << type << ", name: \"" << name << "\" }\n";
     }
 };
+
+class FunctionDecl : public ASTNode {
+public:
+    string returnType;
+    string name;
+    vector<unique_ptr<Parameter>> parameters;
+    unique_ptr<ASTNode> body;
+    
+    FunctionDecl(const string& retType, const string& funcName) 
+        : ASTNode(NodeType::FunctionDecl), returnType(retType), name(funcName) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << "Fn(\n";
+        printIndent(indent + 1);
+        cout << "FnDecl {\n";
+        printIndent(indent + 2);
+        cout << "type_tok: " << returnType << ",\n";
+        printIndent(indent + 2);
+        cout << "ident: \"" << name << "\",\n";
+        printIndent(indent + 2);
+        cout << "params: [\n";
+        for (const auto& param : parameters) {
+            param->print(indent + 3);
+        }
+        printIndent(indent + 2);
+        cout << "],\n";
+        printIndent(indent + 2);
+        cout << "block: [\n";
+        if (body) body->print(indent + 3);
+        printIndent(indent + 2);
+        cout << "]\n";
+        printIndent(indent + 1);
+        cout << "}\n";
+        printIndent(indent);
+        cout << ")\n";
+    }
+};
+
+class Block : public ASTNode {
+public:
+    vector<unique_ptr<ASTNode>> statements;
+    
+    Block() : ASTNode(NodeType::Block) {}
+    
+    void print(int indent = 0) const override {
+        for (const auto& stmt : statements) {
+            stmt->print(indent);
+        }
+    }
+};
+
+class VarDecl : public ASTNode {
+public:
+    string type;
+    string name;
+    unique_ptr<ASTNode> initializer;
+    
+    VarDecl(const string& t, const string& n) : ASTNode(NodeType::VarDecl), type(t), name(n) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << "Var(\n";
+        printIndent(indent + 1);
+        cout << "VarDecl {\n";
+        printIndent(indent + 2);
+        cout << "type_tok: " << type << ",\n";
+        printIndent(indent + 2);
+        cout << "ident: \"" << name << "\",\n";
+        printIndent(indent + 2);
+        cout << "expr: ";
+        if (initializer) {
+            cout << "Some(\n";
+            initializer->print(indent + 3);
+            printIndent(indent + 2);
+            cout << ")\n";
+        } else {
+            cout << "None\n";
+        }
+        printIndent(indent + 1);
+        cout << "}\n";
+        printIndent(indent);
+        cout << ")\n";
+    }
+};
+
+class Assignment : public ASTNode {
+public:
+    string variable;
+    unique_ptr<ASTNode> expression;
+    
+    Assignment(const string& var) : ASTNode(NodeType::Assignment), variable(var) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << "Assign(\"" << variable << "\", ";
+        if (expression) expression->print(0);
+        cout << ")\n";
+    }
+};
+
+class BinaryOp : public ASTNode {
+public:
+    string operator_;
+    unique_ptr<ASTNode> left;
+    unique_ptr<ASTNode> right;
+    
+    BinaryOp(const string& op) : ASTNode(NodeType::BinaryOp), operator_(op) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << "::" << getOpName() << "(" << operator_ << ")\n";
+        if (left) left->print(indent + 1);
+        if (right) right->print(indent + 1);
+    }
+    
+private:
+    string getOpName() const {
+        if (operator_ == "+") return "Add";
+        if (operator_ == "-") return "Sub";
+        if (operator_ == "*") return "Mul";
+        if (operator_ == "/") return "Div";
+        if (operator_ == "==") return "Comp";
+        if (operator_ == "!=") return "Comp";
+        if (operator_ == "<") return "Comp";
+        if (operator_ == ">") return "Comp";
+        if (operator_ == "<=") return "Comp";
+        if (operator_ == ">=") return "Comp";
+        return "BinOp";
+    }
+};
+
+class UnaryOp : public ASTNode {
+public:
+    string operator_;
+    unique_ptr<ASTNode> operand;
+    
+    UnaryOp(const string& op) : ASTNode(NodeType::UnaryOp), operator_(op) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << "Unary(" << operator_ << ")\n";
+        if (operand) operand->print(indent + 1);
+    }
+};
+
+class FunctionCall : public ASTNode {
+public:
+    string functionName;
+    vector<unique_ptr<ASTNode>> arguments;
+    
+    FunctionCall(const string& name) : ASTNode(NodeType::FunctionCall), functionName(name) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << "Call(FnCall {\n";
+        printIndent(indent + 1);
+        cout << "ident: \"" << functionName << "\",\n";
+        printIndent(indent + 1);
+        cout << "args: [\n";
+        for (const auto& arg : arguments) {
+            printIndent(indent + 2);
+            cout << "Some(\n";
+            arg->print(indent + 3);
+            printIndent(indent + 2);
+            cout << "),\n";
+        }
+        printIndent(indent + 1);
+        cout << "]\n";
+        printIndent(indent);
+        cout << "})\n";
+    }
+};
+
+class Identifier : public ASTNode {
+public:
+    string name;
+    
+    Identifier(const string& n) : ASTNode(NodeType::Identifier), name(n) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << "\"" << name << "\"\n";
+    }
+};
+
+class IntLiteral : public ASTNode {
+public:
+    int value;
+    
+    IntLiteral(int v) : ASTNode(NodeType::IntLiteral), value(v) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << value << "\n";
+    }
+};
+
+class FloatLiteral : public ASTNode {
+public:
+    float value;
+    
+    FloatLiteral(float v) : ASTNode(NodeType::FloatLiteral), value(v) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << value << "\n";
+    }
+};
+
+class StringLiteral : public ASTNode {
+public:
+    string value;
+    
+    StringLiteral(const string& v) : ASTNode(NodeType::StringLiteral), value(v) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << "\"" << value << "\"\n";
+    }
+};
+
+class ReturnStmt : public ASTNode {
+public:
+    unique_ptr<ASTNode> expression;
+    
+    ReturnStmt() : ASTNode(NodeType::ReturnStmt) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << "Ret(\n";
+        printIndent(indent + 1);
+        cout << "ExprStmt {\n";
+        printIndent(indent + 2);
+        cout << "expr: ";
+        if (expression) {
+            cout << "Some(\n";
+            expression->print(indent + 3);
+            printIndent(indent + 2);
+            cout << ")\n";
+        } else {
+            cout << "None\n";
+        }
+        printIndent(indent + 1);
+        cout << "}\n";
+        printIndent(indent);
+        cout << ")\n";
+    }
+};
+
+class IfStmt : public ASTNode {
+public:
+    unique_ptr<ASTNode> condition;
+    unique_ptr<ASTNode> thenBranch;
+    unique_ptr<ASTNode> elseBranch;
+    
+    IfStmt() : ASTNode(NodeType::IfStmt) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << "If(\n";
+        printIndent(indent + 1);
+        cout << "IfStmt {\n";
+        printIndent(indent + 2);
+        cout << "cond: ";
+        if (condition) {
+            cout << "Some(\n";
+            condition->print(indent + 3);
+            printIndent(indent + 2);
+            cout << "),\n";
+        } else {
+            cout << "None,\n";
+        }
+        printIndent(indent + 2);
+        cout << "if_block: [\n";
+        if (thenBranch) thenBranch->print(indent + 3);
+        printIndent(indent + 2);
+        cout << "],\n";
+        printIndent(indent + 2);
+        cout << "else_block: [\n";
+        if (elseBranch) elseBranch->print(indent + 3);
+        printIndent(indent + 2);
+        cout << "]\n";
+        printIndent(indent + 1);
+        cout << "}\n";
+        printIndent(indent);
+        cout << ")\n";
+    }
+};
+
+class WhileStmt : public ASTNode {
+public:
+    unique_ptr<ASTNode> condition;
+    unique_ptr<ASTNode> body;
+    
+    WhileStmt() : ASTNode(NodeType::WhileStmt) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << "While(\n";
+        printIndent(indent + 1);
+        cout << "WhileStmt {\n";
+        printIndent(indent + 2);
+        cout << "cond: ";
+        if (condition) {
+            cout << "Some(\n";
+            condition->print(indent + 3);
+            printIndent(indent + 2);
+            cout << "),\n";
+        }
+        printIndent(indent + 2);
+        cout << "body: [\n";
+        if (body) body->print(indent + 3);
+        printIndent(indent + 2);
+        cout << "]\n";
+        printIndent(indent + 1);
+        cout << "}\n";
+        printIndent(indent);
+        cout << ")\n";
+    }
+};
+
+class PrintStmt : public ASTNode {
+public:
+    vector<unique_ptr<ASTNode>> arguments;
+    
+    PrintStmt() : ASTNode(NodeType::PrintStmt) {}
+    
+    void print(int indent = 0) const override {
+        printIndent(indent);
+        cout << "Print([\n";
+        for (const auto& arg : arguments) {
+            arg->print(indent + 1);
+        }
+        printIndent(indent);
+        cout << "])\n";
+    }
+};
