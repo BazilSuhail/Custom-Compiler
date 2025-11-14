3ae5e5a92c51a5b2f2e1f03fca0cd639b6d55d65
BazilSuhail <bazil1854@gmail.com>
2025-11-14 23:58:55 +0500

    Udate type-checker's functionality linked to global symbol table.

--- scope.cpp
+++ scope.cpp
@@ -2,8 +2,117 @@
 #include <stack>
 #include <set>
 
+// === Scope Analysis Errors ===
+enum class ScopeErrorType {
+    UndeclaredVariableAccessed,
+    UndefinedFunctionCalled,
+    VariableRedefinition,
+    FunctionPrototypeRedefinition,
+    ConflictingFunctionDefinition,
+    ConflictingDeclaration,
+    ParameterRedefinition,
+    InvalidForwardReference,
+    InvalidStorageClassUsage,
+    EnumRedefinition,
+    EnumVariantRedefinition,
+};
 
+struct ScopeError {
+    ScopeErrorType type;
+    string name;
+    int line;
+    int column;
+    string message;
 
+    ScopeError(ScopeErrorType t, const string& n, int l, int c) 
+        : type(t), name(n), line(l), column(c) {
+        switch (t) {
+            case ScopeErrorType::UndeclaredVariableAccessed:
+                message = "Undeclared variable accessed: '" + n + "'";
+                break;
+            case ScopeErrorType::UndefinedFunctionCalled:
+                message = "Undefined function called: '" + n + "'";
+                break;
+            case ScopeErrorType::VariableRedefinition:
+                message = "Variable redefinition: '" + n + "'";
+                break;
+            case ScopeErrorType::FunctionPrototypeRedefinition:
+                message = "Function prototype redefinition: '" + n + "'";
+                break;
+            case ScopeErrorType::ConflictingFunctionDefinition:
+                message = "Conflicting function definition: '" + n + "'";
+                break;
+            case ScopeErrorType::ConflictingDeclaration:
+                message = "Conflicting declaration: '" + n + "'";
+                break;
+            case ScopeErrorType::ParameterRedefinition:
+                message = "Parameter redefinition: '" + n + "'";
+                break;
+            case ScopeErrorType::InvalidForwardReference:
+                message = "Invalid forward reference: '" + n + "'";
+                break;
+            case ScopeErrorType::InvalidStorageClassUsage:
+                message = "Invalid storage class usage for: '" + n + "'";
+                break;
+            case ScopeErrorType::EnumRedefinition:
+                message = "Enum redefinition: '" + n + "'";
+                break;
+            case ScopeErrorType::EnumVariantRedefinition:
+                message = "Enum variant redefinition: '" + n + "'";
+                break;
+        }
+    }
+};
+
+struct SymbolInfo {
+    TokenType type;
+    string name;
+    int line;
+    int column;
+    bool isFunction;
+    bool isEnum;
+    bool isEnumValue;
+    bool isPrototype; // Distinguishes prototypes from definitions
+    vector<pair<TokenType, string>> params;
+    int scopeLevel; // Added to track scope level
+    
+    SymbolInfo(TokenType t, const string& n, int l, int c, bool isFunc = false, bool isEnumSym = false, bool isEnumVal = false, bool isProto = false, vector<pair<TokenType, string>> p = {}, int scopeLvl = 0)
+        : type(t), name(n), line(l), column(c), isFunction(isFunc), isEnum(isEnumSym), isEnumValue(isEnumVal), isPrototype(isProto), params(p), scopeLevel(scopeLvl) {}
+};
+
+// Structure to represent a scope frame in the symbol table
+struct ScopeInfo {
+    int level;
+    ScopeInfo* parent;
+    unordered_map<string, SymbolInfo> symbols;
+    vector<unique_ptr<ScopeInfo>> children;
+    
+    ScopeInfo(int l = 0, ScopeInfo* p = nullptr) : level(l), parent(p) {}
+    
+    void addSymbol(const SymbolInfo& sym) {
+        symbols.insert({sym.name, sym});
+    }
+    
+    SymbolInfo* findSymbol(const string& name) {
+        auto it = symbols.find(name);
+        if (it != symbols.end()) {
+            return &(it->second);
+        }
+        return nullptr;
+    }
+    
+    const SymbolInfo* findSymbol(const string& name) const {
+        auto it = symbols.find(name);
+        if (it != symbols.end()) {
+            return &(it->second);
+        }
+        return nullptr;
+    }
+    
+    bool hasSymbol(const string& name) const {
+        return symbols.find(name) != symbols.end();
+    }
+};
 
 class ScopeAnalyzer {
 private:
